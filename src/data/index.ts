4f9b89e957ef2d09efd1b01e24c4d482f2c869bc
cieloazul310 <cieloazul310@gmail.com>
2023-10-18 18:46:05 +0900

    chore: update

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,23 +1,21 @@
-import { defineMenu } from "@cieloazul310/astro-sarkara";
+import { defineSiteMetadata, defineMenu } from "@cieloazul310/astro-sarkara";
 
-export const siteMetadata = {
+export const siteMetadata = defineSiteMetadata({
 	title: "水戸地図(β)",
 	description: "水戸地図のAstroバージョン",
-  image: "./ogimage.png",
-};
+  image: "/ogimage.png",
+  favicon: "/favicon.ico",
+});
 export const menu = defineMenu([
-  { title: "Top", href: "/" },
-  { title: "Posts", href: "/posts" },
+  { title: "トップページ", href: "/" },
+  { title: "記事一覧", href: "/posts" },
   {
-    title: "Link",
+    title: "技術記事",
     items: [
-      { title: "Astro", href: "https://astro.build/" },
-      { title: "Panda CSS", href: "https://panda-css.com/" },
-      {
-        title: "GitHub Repo",
-        href: "https://github.com/cieloazul310/astro-sarkara",
-      },
+      { title: "技術記事", href: "/tech" },
+      { title: "Astro", href: "/tech/astro" },
+      { title: "Gatsby", href: "/tech/gatsby" },
     ],
   },
 ]);
-export const pageSize = 25;
+export const pageSize = 1;
